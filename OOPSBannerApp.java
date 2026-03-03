914c23f787b2c8654f434ccfc1627b5fbff3ecdc
saheer077 <dsahee41@gmail.com>
2026-03-03 13:58:03 +0530

    UC5: Array declaration+init with String.join() enhanced for loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,20 +1,26 @@
-/**
- * OOPSBannerApp - UC2
- * Displays "OOPS" in a 7-line ASCII banner format.
- *
- * @author Saheer
- * @version 2.0
- */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****    *****    *****   ***** ");
-        System.out.println("*     *  *     *  *     *  *     ");
-        System.out.println("*     *  *     *  *     *  *     ");
-        System.out.println("*     *  *     *  *****    ***** ");
-        System.out.println("*     *  *     *  *            * ");
-        System.out.println("*     *  *     *  *            * ");
-        System.out.println(" *****    *****   *        ***** ");
+        System.out.println(String.join(" ",
+                "*****",  " *****",  "*****",  "*****"));
+
+        System.out.println(String.join(" ",
+                "*   *", "*   *", "*   *", "*"));
+
+        System.out.println(String.join(" ",
+                "*   *", "*   *", "*   *", "*"));
+
+        System.out.println(String.join(" ",
+                "*   *", "*****", "*****", "*****"));
+
+        System.out.println(String.join(" ",
+                "*   *", "*   *", "*     ", "*   *"));
+
+        System.out.println(String.join(" ",
+                "*   *", "*   *", "*     ", "*   *"));
+
+        System.out.println(String.join(" ",
+                "*****", " *****", "*     ", "*****"));
     }
 }
